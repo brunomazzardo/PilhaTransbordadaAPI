5f50282784fc8e70586673c92fd583141c9b3d6e
brunomazzardo <bruno.mazzardo52@gmail.com>
2018-07-01 01:38:59 -0300

    Ajustes build heroku porta

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ const express = require("express")
 // Create Express server
 const app = express()
 import  Auth  from "./Util/Auth"
+import { sequelize } from "./db"
 // Express configuration
 
 app.use(bodyParser.json())
@@ -16,6 +17,14 @@ app.use(Auth.validate)
 /**
  * Primary app routes.
  */
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.")
+  })
+  .catch((err: Error) => {
+    console.error("Unable to connect to the database:", err)
+  })
 app.get("/", login )
 
 app.listen(process.env.PORT || 3000, () => console.log("Oi gente"))
---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+const pg = require("pg")
+pg.defaults.ssl = true
+const Sequelize = require("sequelize")
+
+// Or you can simply use a connection uri
+const sequelize = new Sequelize("d8omdtlehhme65", "wmaydkrnbqjwun", "756188cc6084815dae77cd14521b99e658cff5743179774088278dc6892efa9b", {
+  host: "ec2-23-21-201-255.compute-1.amazonaws.com",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+})
+export {sequelize}
